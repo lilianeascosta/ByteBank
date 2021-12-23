00ab8a27d09d5e41e9e99c7c3d46ba0f98b4fea9
lilianeascosta <lilianecosta767@hotmail.com>
2021-12-23 17:33:28 -0300

    making color theme works

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,22 @@ void main() => runApp(
 );
 
 class ByteBankApp extends StatelessWidget {
-  const ByteBankApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      home: Scaffold(
-        body: TransferList(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.green,
+        ).copyWith(
+          secondary: Colors.blueAccent[700],
+        ),
+        buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary
+        ),
       ),
+      home: TransferList(),
     );
   }
 }
