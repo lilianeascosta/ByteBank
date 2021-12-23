1ba469f9c6bb3c9a2d41e11dcd95d44bba17a4ca
lilianeascosta <lilianecosta767@hotmail.com>
2021-12-23 16:36:59 -0300

    form implementation and bugs resolved

--- lib/main.dart
+++ lib/main.dart
@@ -11,43 +11,141 @@ class ByteBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: Scaffold(
-        body: TransferForm(),
+        body: TransferList(),
       ),
     );
   }
 }
 
-class TransferForm extends StatelessWidget {
-  const TransferForm({Key? key}) : super(key: key);
+class Editor extends StatelessWidget {
+
+  final TextEditingController controller;
+  final String label;
+  final String tip;
+  final IconData? icone;
+
+
+  // ignore: use_key_in_widget_constructors
+  const Editor({required this.controller, required this.label, required this.tip, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(
+          icon: icone != null ? Icon(icone) : null,
+          labelText: label,
+          hintText: tip, // tip to the user
+        ),
+        keyboardType: TextInputType.number,
+      ),
+    ) ;
+  }
+}
+
+class TransferForm extends StatefulWidget {
+  //const TransferForm({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TransferFormState();
+  }
+}
+
+class TransferFormState extends State<TransferForm> {
+  final TextEditingController _campControllerAccountNumber = TextEditingController();
+  final TextEditingController _campControllerValue = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Creating Transfers'),),
+      body:
+      Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Editor(controller: _campControllerAccountNumber,
+                label: 'Account Number',
+                tip: '1234',),
+              Editor(controller: _campControllerValue,
+                label: 'Value',
+                tip: '1000.00',
+                icone: Icons.monetization_on,),
+              ElevatedButton(
+                child: Text('Confirm'),
+                onPressed: () => _TransferCreate(context),
+                //child: Text(_textButtonConfirm),
+                //onPressed: () => _TransferCreation(context),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void _TransferCreate(BuildContext context) {
+    final int accountNumber = int.parse(_campControllerAccountNumber.text);
+    final double value = double.parse(_campControllerValue.text);
+    if(accountNumber != null && value != null) {
+      final TransferCreated = Transfer(value, accountNumber);
+      debugPrint('criando transferencia');
+      debugPrint('$TransferCreated');
+      Navigator.pop(context, TransferCreated);
+    }
   }
 }
 
+class TransferList extends StatefulWidget {
 
-class TransferList extends StatelessWidget {
+  final List<Transfer> _transfers = <Transfer>[];
+  // final List<Transfer> _transfers = List();
+
+  @override
+  State<StatefulWidget> createState() {
+    return TransferListState();
+  }
+}
+
+class TransferListState extends State<TransferList>{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text('Transfers'),),
-      body: Column(
-        children: [
-          TransferItem(Transfer(100.00, 123)),
-          TransferItem(Transfer(200.00, 456)),
-          TransferItem(Transfer(300.00, 789)),
-        ],
+      body: ListView.builder(
+        itemCount: widget._transfers.length,
+        itemBuilder: (context, id) {
+          final transfer = widget._transfers[id];
+          return TransferItem(transfer);
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
         child: Icon(Icons.add),
+        onPressed: () {
+          final Future<Transfer?> future = Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return TransferForm();
+          }));
+          future.then((TransferRecieved) {
+            Future.delayed(Duration(seconds: 3), () {
+              if(TransferRecieved != null ) {
+                setState(() {
+                  widget._transfers.add(TransferRecieved);
+                });
+              }
+            });
+          });
+        },
       ),
     );
   }
+
 }
 
 class TransferItem extends StatelessWidget {
@@ -74,3 +172,4 @@ class Transfer {
 
   Transfer(this.value, this.accountNumber);
 }
+
